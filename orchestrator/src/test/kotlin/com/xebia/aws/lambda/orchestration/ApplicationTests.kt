2c1a77e7f356a65cb34779be2c4b41e1f42db1e1
Roy Straub <roy@codecraftr.nl>
2024-02-16 09:42:50 +0100

    Added orchestrator

---
+++ orchestrator/src/test/kotlin/com/xebia/aws/lambda/orchestration/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xebia.aws.lambda.orchestration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
