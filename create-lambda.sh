17ec177139b045d1dc6e4547996a00499a0f1f87
Matthijs Thoolen <mthoolen@xebia.com>
2024-02-16 09:55:13 +0100

    add lambda creation script

---
+++ create-lambda.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+zip function.zip lambda.js
+awslocal lambda create-function \
+    --function-name lambda-part \
+    --runtime nodejs18.x \
+    --zip-file fileb://function.zip \
+    --handler index.handler \
+    --role arn:aws:iam::000000000000:role/lambda-role
